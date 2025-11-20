c0c790b6f46354513a04bb20eb7ff06dbb4fb371
shajanselvam <shajan.selvam@gmail.com>
2025-11-20 22:17:22 +0530

    changed to shajan s

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>SNUC Pro Portal made with love by Shajan Selvam</title>
+  <title>SNUC Pro Portal made with love by Shajan S</title>
   <style>
     body {
       font-family: Arial, sans-serif;
@@ -166,4 +166,5 @@ app.get('/health', (req, res) => {
 app.listen(PORT, () => {
   console.log("SNUC Pro Portal running on port " + PORT);
 });
+
 
